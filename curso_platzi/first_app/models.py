cfcd2358ed944d9550fbc9baff5ce1ed8eff8b94
FFon0711 <ff010711@gmail.com>
2024-10-14 23:04:55 -0400

    Add fields to Car

--- curso_platzi/first_app/models.py
+++ curso_platzi/first_app/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Car(models.Model):
     title = models.TextField(max_length=250)
     year = models.TextField(max_length=4, null=True)
+    color = models.TextField(max_length=20, null=True)
+    propetaries = models.IntegerField(null=True)
